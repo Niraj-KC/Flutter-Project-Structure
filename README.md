fc9b4113d4abe7c27130a2e8a9f1a1acdfb9e01c
Niraj kc <niraj.kc.128@gmail.com>
2023-10-04 21:22:33 +0530

    Project Structure

---
+++ README.md
@@ -0,0 +1,31 @@
+# **Rules For Naming**
+
+**Folder/Directory:** DemoFolderName
+
+**class files:** DemoClassName.\* (file name same as class name)
+**other file:** demoOtherFileName.\*
+
+**class name:** DemoClassName (in file [DemoClassName.\*])
+**function name:** demoFunctionName()
+
+**global constant variable:** DEMO_GLOBAL_VAR
+**class and function variable:** demoVar
+
+# Project Structure
+
+#### lib
+- Models
+- Screens
+    - Auth
+    - UserType1
+        - Home.dart
+    - UserType2
+        - Home.dart
+    - SplashScreen.dart
+- Services
+    - Auth
+- Utils
+    - CustomWidgets
+    - APIHelper.dart
+    - constant.dart
+- main.dart
---
+++ Util/APIHelper.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class APIHelper{
+  // ignore: constant_identifier_names
+  static const String BASE_URI = "http://172.20.10.3:8070/";
+
+  static Future post(
+      String uri,
+      dynamic body,
+      {
+        Map<String, String>header = const <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        String errorMsg = "Failed",
+      }
+      )
+  async {
+    final res = await http.post(
+      Uri.parse(BASE_URI+uri),
+      headers: header,
+      body: body,
+    );
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception(errorMsg);
+    }
+
+  }
+
+
+}
